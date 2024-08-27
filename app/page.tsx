d47b50f5c12934c0e3be6bdff3e5ec0928cb63f2
1170026 <shiori.watanabe@toda.co.jp>
2024-08-27 09:20:28 +0900

    add tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ export default function Page() {
         {/* <AcmeLogo /> */}
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
+      <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"/>
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
