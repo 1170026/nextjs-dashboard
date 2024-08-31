1ed63cfdc1ccb09efd6ef4a1b6dbc907edcb26fb
1170026 <shiori.watanabe@toda.co.jp>
2024-08-31 12:43:24 +0900

    遅いデータ取得シミュレート

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -16,8 +16,8 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     // sql:SQLクエリの実行
     // <Revenue>:型指定
@@ -26,7 +26,7 @@ export async function fetchRevenue() {
     // revenue テーブルの全てのデータを検索し、その結果を返す
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
