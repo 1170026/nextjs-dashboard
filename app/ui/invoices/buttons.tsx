8aff5d60e56c25c1fa55e74893e1704e9f772c56
1170026 <shiori.watanabe@toda.co.jp>
2024-09-01 22:52:24 +0900

    特定の請求書を取得する

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -16,7 +16,8 @@ export function CreateInvoice() {
 export function UpdateInvoice({ id }: { id: string }) {
   return (
     <Link
-      href="/dashboard/invoices"
+      // href="/dashboard/invoices"
+      href ={`/dashboard/invoices/${id}/edit`} //アップデート時にパスの変更
       className="rounded-md border p-2 hover:bg-gray-100"
     >
       <PencilIcon className="w-5" />
