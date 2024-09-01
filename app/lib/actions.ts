ea32244375c307debf41694c03a024c4067ac9d8
1170026 <shiori.watanabe@toda.co.jp>
2024-09-01 21:44:10 +0900

    データ型の検証

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,11 +2,23 @@
 // サーバー アクションは、クライアント側（ブラウザ）から呼び出してサーバー側で実行される関数
 'use server';
 
+import { z } from "zod"; //スキーマからデータの検証を行うためのライブラリ
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(), //文字列から数値への変換
+    status: z.enum(['pending', 'paid']), //'pending'または'paid'のいずれか
+    date: z.string(),
+})
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
-    const rawFormData = {
+    const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
-    }
-    console.log(rawFormData);
+    });
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
 }
