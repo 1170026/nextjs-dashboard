f77a7c7f2a7f828a1af705eb0a5049232effc494
1170026 <shiori.watanabe@toda.co.jp>
2024-09-05 08:43:15 +0900

    lib/action.tsにtry/catchの追加

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -29,10 +29,16 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
-    await sql`
-    INSERT INTO invoices (customer_id,amount,status, status, date)
-    VALUES (${customerId},${amountInCents},${status}, ${date})
-    `;
+    try {
+        await sql`
+        INSERT INTO invoices (customer_id,amount,status, status, date)
+        VALUES (${customerId},${amountInCents},${status}, ${date})
+        `;
+    } catch (error) {
+        return {
+            message: 'Database Error: Failed to Create Invoice.'
+        }
+    }
 
     // sqlテンプレートリテラルでデータベースに請求書情報を挿入するSQLクエリを実行
     // customerId、amountInCents、status、dateが対応するフィールドに挿入
@@ -48,17 +54,30 @@ export async function UpdateInvoice(id: string, formData: FormData) {
     })
     const amountInCents = amount * 100;
 
-    await sql`
-        UPDATE invoices
-        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-        WHERE id = ${id}
-    `;
+    try {
+        await sql`
+            UPDATE invoices
+            SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+            WHERE id = ${id}
+        `;
+
+    } catch (error) {
+        return {
+            message: 'Database Error: Failed to Updata Invoice.'
+        }
+    }
     
     revalidatePath('/dashboard/invoices');//ページキャッシュのクリア
     redirect('/dashboard/invoices');//検索後にリダイレクト
 }
 export async function deleteInvoice(id: string) {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-
-    revalidatePath('/dashboard/invoices');
-  }
+    try {
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
+        revalidatePath('/dashboard/invoices');
+        return { message: 'Deleted Invoice' };
+    } catch (error) {
+        return {
+            message: 'Database Error: Failed to Delete Invoice.'
+        }
+    }
+}
