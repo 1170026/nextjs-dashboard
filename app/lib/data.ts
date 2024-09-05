22fcbf5d44d8c7399301bf1fe385d8e06036ce4c
1170026 <shiori.watanabe@toda.co.jp>
2024-09-05 09:23:16 +0900

    404not-foundによるエラー表示とリダイレクト

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -166,7 +166,7 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
-
+    console.log(invoice) // Invoiceは空の行列
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
